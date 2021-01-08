1925cffb178681b749c961132d2761ce56beab4b
dchhitarka <dchhitarka@gmail.com>
2021-01-08 22:13:09 +0530

    Removed unwanted files

--- venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/dchhitarka/Desktop/Web Development/WebDev wih Python/Django/CollegeBuddy/venv/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
